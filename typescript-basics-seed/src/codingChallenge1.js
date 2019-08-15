74d6d06f47dce481a03f9d74c88bd7a621b0b7f6
ploeffler <philiploeffler@gmail.com>
2019-08-15 09:11:33 -0400

    practice

---
+++ typescript-basics-seed/src/codingChallenge1.js
@@ -0,0 +1,19 @@
+
+
+function fizzBuzz(a) {
+    for (let i = 0; i < a; i++) {
+        if (i%3 === 0) {
+        console.log("Fizz")
+        }
+        else if (i%5 === 0) {
+        console.log("Buzz")
+        }
+        else if(i%3 === 0 && i%5 === 0) {
+            console.log("FizzBuzz")
+        }
+        else {
+            console.log("my ashhhh")
+        }
+    }
+}
+fizzBuzz(4);
